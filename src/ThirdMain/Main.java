80c465e56a2783e33e8f24a1a27cc08a847df9de
Oleksiy Beseda <beseda.odessa@gmail.com>
2023-09-11 10:05:10 +0300

    hw11a

---
+++ src/ThirdMain/Main.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем HashMap
+        Map<String, Integer> map = new HashMap<>();
+
+        // Заполняем HashMap парами ключ-значение
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("Three", 3);
+        map.put("Four", 4);
+        map.put("Five", 5);
+        map.put("Six", 6);
+        map.put("Seven", 7);
+        map.put("Eight", 8);
+        map.put("Nine", 9);
+        map.put("Ten", 10);
+
+        // Выводим содержимое HashMap
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
